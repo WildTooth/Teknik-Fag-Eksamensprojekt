e3aba977a0bc0fd8c4ddb31c694978e102b0bcac
Jonathan Holck <97985568+WildTooth@users.noreply.github.com>
2025-03-06 11:14:59 +0100

    Oprettet global.h

---
+++ global.h
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern const int LIGHT_PIN_YELLOW;
+extern const int LIGHT_PIN_BLUE;
+extern const int LIGHT_PIN_RED;
+extern const int LIGHT_PIN_GREEN;
+
+#endif
