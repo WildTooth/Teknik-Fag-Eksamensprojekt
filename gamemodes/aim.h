8aa3f8a8a815afef15ef1bd4e0b2ca369bbbd59d
Jonathan Holck <97985568+WildTooth@users.noreply.github.com>
2025-03-06 18:15:07 +0100

    Tilføjet aim.h

---
+++ gamemodes/aim.h
@@ -0,0 +1,76 @@
+#ifndef AIM_H
+#define AIM_H
+
+#include "gamemode.h"
+#include "../global.h"
+
+void playWinningSound() {
+    tone(BUZZER_PIN,261);
+    delay(100);
+    tone(BUZZER_PIN,349);
+    delay(100);
+    tone(BUZZER_PIN,220);
+    delay(100);
+    tone(BUZZER_PIN,174);
+    delay(100);
+    tone(BUZZER_PIN,261);
+    delay(400);
+    noTone(BUZZER_PIN);
+}
+
+void playLosingSound() {
+    tone(BUZZER_PIN,261);
+    delay(200);
+    tone(BUZZER_PIN,277);
+    delay(200);
+    tone(BUZZER_PIN,220);
+    delay(200);
+    tone(BUZZER_PIN,155);
+    delay(200);
+    tone(BUZZER_PIN,138);
+    delay(400);
+    noTone(BUZZER_PIN);
+}
+
+void doRound() {
+    digitalWrite(LIGHT_PIN_RED, LOW);
+    int throwPause = randomInclusive(1000, 5000);
+    int throwWindow = randomInclusive(3000, 6000);
+
+    for (int i = 0; i < throwPause; i++) {
+        if (digitalRead(MICROPHONE_PIN) == HIGH) {
+            playLosingSound();
+            delay(1);
+        }
+    }
+
+    long startEpoch = millis();
+    while (millis() - startEpoch < throwWindow) {
+        if ((millis() - startEpoch) % 100 == 1) {
+            digitalWrite(LIGHT_PIN_RED, HIGH);
+        } else if ((millis() - startEpoch) % 100 == 50) {
+            digitalWrite(LIGHT_PIN_RED, LOW);
+        }
+        if (digitalRead(MICROPHONE_PIN) == HIGH) {
+            digitalWrite(LIGHT_PIN_BLUE, HIGH);
+            playWinningSound();
+            delay(500);
+            digitalWrite(LIGHT_PIN_BLUE,LOW);
+            break;
+        }
+    }
+}
+
+class AimGame : public Gamemode {
+public:
+
+    void init() override {
+        Gamemode::init();
+    }
+    void run() override {
+        Gamemode::run();
+        doRound();
+    }
+};
+
+#endif
