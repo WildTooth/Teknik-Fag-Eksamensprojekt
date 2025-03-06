adfba0a534fd697e15c6f13d697000e7bac639bb
Jonathan Holck <97985568+WildTooth@users.noreply.github.com>
2025-03-06 11:45:37 +0100

    Tilføjet flere globale variabler samt den nye #size(array[]) funktion.

--- global.cpp
+++ global.cpp
@@ -4,3 +4,6 @@ const int LIGHT_PIN_YELLOW = 0;
 const int LIGHT_PIN_BLUE = 0;
 const int LIGHT_PIN_RED = 0;
 const int LIGHT_PIN_GREEN = 0;
+
+const int LIGHT_PINS[] = {LIGHT_PIN_YELLOW, LIGHT_PIN_BLUE, LIGHT_PIN_RED, LIGHT_PIN_GREEN};
+const int LIGHT_PIN_AMOUNT = size(LIGHT_PINS);
--- global.h
+++ global.h
@@ -1,9 +1,19 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#include <stddef.h> // Arduinos definitioner, vi skal bruge dette import for at kunne bruge typen "size_t"
+
+template <typename T, size_t N>
+constexpr int size(T (&array)[N]) {
+  return N;
+}
+
 extern const int LIGHT_PIN_YELLOW;
 extern const int LIGHT_PIN_BLUE;
 extern const int LIGHT_PIN_RED;
 extern const int LIGHT_PIN_GREEN;
+
+extern const int LIGHT_PINS[];
+extern const int LIGHT_PIN_AMOUNT;
 
 #endif
