a6b86cee57e235491d9d94ec24e6c0bc2fbabad9
fant03 <silas.autzen@gmail.com>
2025-03-13 10:11:05 +0100

    Konfigureret global.h til at have pinsne til vores test produkt

--- global.h
+++ global.h
@@ -13,27 +13,27 @@ long randomInclusive(long min, long max) {
   return random(min, max + 1);
 }
 
-const int LIGHT_PIN_YELLOW = 0;
-const int LIGHT_PIN_BLUE = 0;
-const int LIGHT_PIN_RED = 0;
-const int LIGHT_PIN_GREEN = 0;
+const int LIGHT_PIN_YELLOW = 23;
+const int LIGHT_PIN_BLUE = 19;
+const int LIGHT_PIN_RED = 22;
+const int LIGHT_PIN_GREEN = 21;
 
 const int LIGHT_PINS[] = {LIGHT_PIN_YELLOW, LIGHT_PIN_BLUE, LIGHT_PIN_RED, LIGHT_PIN_GREEN};
 const int LIGHT_PIN_AMOUNT = size(LIGHT_PINS);
 
-const int BUZZER_PIN = 0;
+const int BUZZER_PIN = 16;
 
 // INPUTS
 
-const int BUTTON_PIN_UP = 0;
-const int BUTTON_PIN_DOWN = 10;
-const int BUTTON_PIN_LEFT = 20;
-const int BUTTON_PIN_RIGHT = 30;
-const int BUTTON_PIN_CENTER = 40;
+const int BUTTON_PIN_UP = 25;
+const int BUTTON_PIN_DOWN = 5;
+const int BUTTON_PIN_LEFT = 18;
+const int BUTTON_PIN_RIGHT = 27;
+const int BUTTON_PIN_CENTER = 26;
 
 const int BUTTON_PINS[] = {BUTTON_PIN_UP, BUTTON_PIN_DOWN, BUTTON_PIN_LEFT, BUTTON_PIN_RIGHT, BUTTON_PIN_CENTER};
 const int BUTTON_PIN_AMOUNT = size(BUTTON_PINS);
 
-const int MICROPHONE_PIN = 0;
+const int MICROPHONE_PIN = 14;
 
 #endif
